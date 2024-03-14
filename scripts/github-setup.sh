3c207bff74704b7c3b21aa17a459157ac14a4b6b
Christian Mahnke <cmahnke@gmail.com>
2024-03-14 22:18:22 +0100

    Add Saxon installation

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -7,4 +7,5 @@ $CTX_PATH/../themes/projektemacher-base/scripts/github/jxl-0.9.1.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/docker-images.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-inkscape.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/setup-xslt.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/dart-sass.sh
